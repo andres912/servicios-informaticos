bfe7ce1df098cfb96d794082de256f23b94cd562
Andy <anfernandez@fi.uba.ar>
2022-06-22 19:24:47 -0300

    import fix

--- project/models/change.py
+++ project/models/change.py
@@ -1,6 +1,6 @@
 from project import db
 from project.models.base_model import BaseModel, NullBaseModel
-from project.models.exceptions import ObjectCreationException
+from project.models.exceptions import ChangeApplicationError, ObjectCreationException
 from project.models.priority import *
 from project.models.solvable import Solvable
 from project.models.status import *
@@ -70,35 +70,6 @@ class Change(Solvable):
             raise ObjectCreationException(
                 object="Incident", cause="No configuration items provided"
             )
-
-
-    def _update(
-        self,
-        title: str = None,
-        description: str = None,
-        priority: str = None,
-        status: str = None,
-        taken_by: str = None,
-    ) -> None:
-        if title:
-            self.title = title
-        if description:
-            self.description = description
-        if priority:
-            self.priority = priority
-        if status:
-            self.status = status
-        if taken_by:
-            self.taken_by = taken_by
-
-    def change_status(self, status: str) -> None:
-        self._update(self, status=status)
-
-    def change_priority(self, priority: str) -> None:
-        self._update(self, priority=priority)
-
-    def assign_user(self, taken_by: str) -> None:
-        self._update(self, taken_by=taken_by)
     
     def verify_problems_incidents(self, incidents: list, problems: list) -> None:
         if not problems and not incidents:
